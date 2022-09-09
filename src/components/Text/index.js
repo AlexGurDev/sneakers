570c35f1c8adc088b573599beb7e285f6a47529c
huraliaksandr <a.hur@melsoft-games.com>
2022-09-09 12:49:49 +0300

    test

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Text = ({ href, text, classNames, Tag }) => {
   console.log("test");
+  console.log("test");
   return (
     <Tag href={href} className={classNames}>
       {text}
